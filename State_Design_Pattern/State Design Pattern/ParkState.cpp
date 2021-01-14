87482458c6c6840a77ed5b6ca0f615e1d4a8128c
Bryan Ching <bryanching7373@gmail.com>
2021-01-14 00:51:22 -0700

    add ParkState logic

--- State_Design_Pattern/State Design Pattern/ParkState.cpp
+++ State_Design_Pattern/State Design Pattern/ParkState.cpp
@@ -1,21 +1,30 @@
 #include "ParkState.h"
 
-void ParkState::shutoffEngine() {
+ParkState::ParkState(Car newCar) {
+    car = newCar;
+}
 
+void ParkState::shutoffEngine() {
+    if (car.handbrakeEngaged == true) {
+        std::cout << "Shutting off engine" << std::endl;
+    }
+    else {
+        std::cout << "Engage the handbrake" << std::endl;
+    }
 }
 
 void ParkState::reverseCar() {
-
+    std::cout << "Put the car in reverse" << std::endl;
 }
 
 void ParkState::pushCar() {
-
+    std::cout << "Car won't budge...put the car in neutral" << std::endl;
 }
 
 void ParkState::accelerateCar() {
-
+    std::cout << "Put the car in drive" << std::endl;
 }
 
 void ParkState::applyBrakes() {
-
+    std::cout << "The car is not moving" << std::endl;
 }
--- State_Design_Pattern/State Design Pattern/ParkState.h
+++ State_Design_Pattern/State Design Pattern/ParkState.h
@@ -2,15 +2,22 @@
 #define PARKSTATE_H
 
 #include "CarState.h"
+#include "Car.h"
 
 class ParkState : public CarState {
     
 public:
+    ParkState(Car newCar);
+    ~ParkState();
+
     void shutoffEngine();
     void reverseCar();
     void pushCar();
     void accelerateCar();
     void applyBrakes();
+
+    Car car;
+
 
 };
 
