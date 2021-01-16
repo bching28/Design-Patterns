c90c5b3461af270904200ec186e87be2f40607bc
Bryan Ching <bryanching7373@gmail.com>
2021-01-16 14:51:47 -0700

    add test cases for testing State Design Pattern

--- State_Design_Pattern/State Design Pattern/Car.cpp
+++ State_Design_Pattern/State Design Pattern/Car.cpp
@@ -4,6 +4,12 @@
 #include "NeutralState.h"
 #include "DriveState.h"
 
+//------------------------------------------------------------------
+// Assumptions:
+// 1. Engage handbrake before turning off engine
+// 2. Engage handbrake can occur when in any state technically
+//------------------------------------------------------------------
+
 Car::Car() {
     parkState = new ParkState(this);
     reverseState = new ReverseState(this);
@@ -74,11 +80,15 @@ void Car::turnEngineOff() {
     if (speed != 0) {
         std::cout << "Car is in motion...stop the car!" << std::endl;
     }
+    else if (carState != getParkCarState()) {
+        std::cout << "Shift the car to Park" << std::endl;
+    }
     else if (handbrakeEngaged == false) {
-        std::cout << "Engage handbrake before turning on the engine" << std::endl;
+        std::cout << "Engage handbrake before turning off the engine" << std::endl;
     }
     else {
         engineOn = false;
+        std::cout << "Turning off engine" << std::endl;
     }
 }
 
@@ -93,31 +103,56 @@ void Car::engageHandbrake() {
 }
 
 void Car::disengageHandbrake() {
+    // Implied that speed is 0 if you are trying to disengage the handbrake
     handbrakeEngaged = false;
     std::cout << "Handbrake disengaged" << std::endl;
 }
 
 void Car::reverseCar(int reverseSpeed) {
-    if (carState == getReverseCarState()) {
-        speed = reverseSpeed;
-        std::cout << "Reversing at " << speed << " mph" << std::endl;
+    if (carState != getReverseCarState()) {
+        std::cout << "Shift the car to Reverse" << std::endl;
+    }
+    else if (handbrakeEngaged == true) {
+        std::cout << "Disengage handbrake" << std::endl;
     }
     else {
-        std::cout << "Shift the car to Reverse" << std::endl;
+        speed = reverseSpeed;
+        std::cout << "Reversing at " << speed << " mph" << std::endl;
     }
 }
 
 void Car::pushCar(int pushDistance) {
-    distance = pushDistance;
-    std::cout << "Pushing car at " << distance << "ft" << std::endl;
+    if (carState != getNeutralCarState()) {
+        std::cout << "Shift the car to Neutral" << std::endl;
+    }
+    else if (handbrakeEngaged == true) {
+        std::cout << "Disengage handbrake" << std::endl;
+    }
+    else {
+        distance = pushDistance;
+        std::cout << "Pushing car " << distance << "ft" << std::endl;
+    }
 }
 
 void Car::accelerateCar(int newSpeed) {
-    speed = newSpeed;
-    std::cout << "Going " << speed << " mph" << std::endl;
+    if (carState != getDriveCarState()) {
+        std::cout << "Shift the car to Drive" << std::endl;
+    }
+    else if (handbrakeEngaged == true) {
+        std::cout << "Disengage handbrake" << std::endl;
+    }
+    else {
+        speed = newSpeed;
+        std::cout << "Going " << speed << " mph" << std::endl;
+    }
 }
 
 void Car::applyBrakes() {
-    speed = 0;
-    std::cout << "Going " << speed << " mph" << std::endl;
+    if (speed > 0) {
+        speed = 0;
+        std::cout << "Going " << speed << " mph" << std::endl;
+    }
+    else if (speed == 0) {
+        std::cout << "Car isn't even moving" << std::endl;
+    }
 }
