4046669f956b302f4e819169f2c74fbeb017c6ce
Bryan Ching <bryanching7373@gmail.com>
2021-02-07 11:51:51 -0700

    add main function for testing

---
+++ Factory_Design_Pattern/Factory_Design_C++/TestEnemyCharacter.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <ios>
+//#include "EnemyCharacter.h"
+#include "EnemyCharacterFactory.h"
+//#include "SwordEnemyCharacter.h"
+//#include "ShieldEnemyCharacter.h"
+//#include "SpearEnemyCharacter.h"
+//#include "BowEnemyCharacter.h"
+
+void executeEnemyPhase(EnemyCharacter* enemyCharacter);
+
+int main() {
+    EnemyCharacterFactory* enemyCharacterFactory = new EnemyCharacterFactory();
+    EnemyCharacter* enemyCharacter = NULL;
+
+    std::string userInput = "";
+
+    while (1) {
+        std::cout << "Select an Enemy Character (sword / shield / spear / bow)" << std::endl;
+        std::cin >> userInput;
+
+        if (userInput == "exit") {
+            std::cout << "Ending programing" << std::endl;
+            break;
+        }
+
+        enemyCharacter = enemyCharacterFactory->makeEnemyCharacter(userInput);
+
+        if (enemyCharacter != NULL) {
+            executeEnemyPhase(enemyCharacter);
+        }
+        else {
+            std::cout << "Enter a valid string next time" << std::endl;
+        }
+
+        // discard the input buffer
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+}
+
+void executeEnemyPhase(EnemyCharacter* enemyCharacter) {
+    enemyCharacter->displayEnemyCharacter();
+    enemyCharacter->followHeroCharacter();
+    enemyCharacter->attackHeroCharacter();
+    std::cout << std::endl;
+}
